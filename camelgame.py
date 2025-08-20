297ce192fbd795e5c10d768f98862492964c3731
theschoolacc <aria.khanpour@student.horizonhonors.org>
2025-08-20 09:30:06 -0700

    Started base of the camel game, check README for more instructions.

--- camelgame.py
+++ camelgame.py
@@ -1,3 +1,11 @@
 import random
 
 class CamelGame:
+    print("Welcome to the Camel Game...")
+    gather_speed = int(input("To start, type a speed (MPH)..."))
+    
+    if gather_speed >= 20:
+        print("Too fast for a camel, please choose a speed under 20MPH.")
+    
+    else:
+        print(f"Sound good, confirm {gather_speed}"...)
