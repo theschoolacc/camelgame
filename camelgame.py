8111e26d7db1d471811dc4d33342a4bfd0ab749c
theschoolacc <aria.khanpour@student.horizonhonors.org>
2025-08-22 09:06:55 -0700

    Refactor game introduction and speed input handling in Camel Game

--- camelgame.py
+++ camelgame.py
@@ -1,11 +1,28 @@
 import random
+import time
+import sys
 
+print("Welcome to the Camel Game!\nYou've taken a camel to journey across the vast Sahara desert. The locals are pursuing you to reclaim their camel. Endure your desert adventure and stay ahead of the natives.")
+
+# Game Starts
 class CamelGame:
-    print("Welcome to the Camel Game...")
-    gather_speed = int(input("To start, type a speed (MPH)..."))
-    
-    if gather_speed >= 20:
-        print("Too fast for a camel, please choose a speed under 20MPH.")
-    
-    else:
-        print(f"Sound good, confirm {gather_speed}"...)
+    print("A. Drink from your canteen.")
+    print("B. Ahead moderate speed.")
+    print("C. Ahead full speed.")
+    print("D. Stop for the night.")
+    print("E. Status check.")
+    print("Q. Quit.")
+    user_choice = input("Your Choice: ").upper()
+
+    if user_choice == "A":
+        print("You drink from your canteen.")
+    elif user_choice == "B":
+        print("You move ahead at moderate speed.")
+    elif user_choice == "C":
+        print("You move ahead at full speed.")
+    elif user_choice == "D":
+        print("You stop for the night.")
+    elif user_choice == "E":
+        print("You check your status.")
+    elif user_choice == "Q":
+        print("You quit the game.")
